04cb43e8d12719b2b2cf42524f7cd4eee6500112
김상현 <kshy1019@naver.com>
2022-04-06 20:45:38 +0900

    LogInView 생성, AppSymbol생성

---
+++ PungDeong/PungDeong/LogIn/AppSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  AppSymbol.swift
+//  PungDeong
+//
+//  Created by 김상현 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct AppSymbol: View {
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
+    var body: some View {
+        Image("풍덩 앱 심볼")
+            .resizable()
+            .frame(width: screenWidth * 0.4, height:  screenWidth * 0.4, alignment: .center)
+            .aspectRatio(contentMode: .fit)
+    }
+}
---
+++ PungDeong/PungDeong/LogIn/LogInView.swift
@@ -0,0 +1,23 @@
+//
+//  LogInView.swift
+//  PungDeong
+//
+//  Created by 김상현 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            
+            AppSymbol()
+            
+            Spacer()
+        }
+    }
+}
