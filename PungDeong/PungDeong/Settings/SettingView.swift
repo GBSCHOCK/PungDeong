17f01e54cce9e980d7fe02273f1215de412b07dc
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-04-07 17:15:23 +0900

    리스트 구현

--- PungDeong/PungDeong/Settings/SettingView.swift
+++ PungDeong/PungDeong/Settings/SettingView.swift
@@ -8,22 +8,31 @@
 import SwiftUI
 
 struct SettingView: View {
+    let menus: [String : String] = ["연결 계정 관리" : "arrow.triangle.2.circlepath","푸시 알림 설정" : "bell","다크 모드 설정" : "macmini","공지사항" : "gear"]
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    Section(header: Text("헤더").frame(width: .infinity).font(.system(size: 30, weight: .bold)).foregroundColor(Color.black)) {
-                        Text("Test1.....")
-                        Text("Test2.....")
-                        Text("Test3.....")
-                        Text("Test4.....")
+            GeometryReader { geometry in
+                VStack {
+                    VStack {
+                        List {
+                            Section(header: ListHeader().frame(width: geometry.size.width, alignment: .leading).background(Color.blue).padding(-6)) {
+                                ForEach(menus.sorted(by: >), id: \.key) { key, value in
+                                    NavigationLink(
+                                        destination: DetailView(data: key)) {
+                                            Image(systemName: value)
+                                                .foregroundColor(Color.blue)
+                                            Text(key)
+                                        }.padding(10)
+                                }
+                            }
+                        }
+                        .listStyle(.plain)
+                        .padding(0)
+                        Spacer()
                     }
+                    .frame(width: .infinity, height: geometry.size.height/1)
+                    Spacer()
                 }
-                .frame(width: .infinity, height: 300)
-                .listStyle(.plain)
-            }
-            VStack {
-                Text("D")
             }
         }
     }
@@ -34,3 +43,25 @@ struct SettingView_Previews: PreviewProvider {
         SettingView()
     }
 }
+
+struct DetailView: View {
+    var data: String
+    var body: some View {
+        VStack {
+            Text(data)
+        }
+    }
+}
+
+struct ListHeader: View {
+    var body: some View {
+        VStack {
+            Text("계정")
+                .foregroundColor(Color.black)
+                .font(.system(size: 25, weight: .bold))
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+        }
+        .frame(height: 50)
+    }
+}
+
