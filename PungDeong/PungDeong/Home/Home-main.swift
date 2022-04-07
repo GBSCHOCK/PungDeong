38124f4966cd49ebc4e7a32eedbb8ac2f2787c9e
김상현 <kshy1019@naver.com>
2022-04-07 20:27:18 +0900

    코드 리뷰

--- PungDeong/PungDeong/Home/Home-main.swift
+++ PungDeong/PungDeong/Home/Home-main.swift
@@ -32,8 +32,11 @@ struct Home_main: View {
                         CardView(image: "작은물개", category: "첫 번째 게임", heading: "명탐정 물개와 함께", explanation: "마을의 평화를 해치는 범인을 찾아보아요!")
                 })
             }
-            .navigationTitle("환영해요!")
-            // .navigationBarTitleDisplayMode(.inline)
+            // 1번 2번 키워드 : toolbar, toolBarItem, navigationBarLeading, navigationBarTrailing
+            
+            
+            //.navigationTitle("환영해요!")
+            //.navigationBarTitleDisplayMode(.inline)
         }
     }
 }
@@ -48,6 +51,10 @@ struct ContinueButton: View {
     }
 }
 
+
+// 카드 뷰가 두개로 나뉘는게 좋을듯 합니다. (frame에 주석 참고)
+// ex) SmallCardView, LargeCardView
+// Home_main에서 HStack 과 VStack을 활용하여 만들어준 카드를 배치해줍니다. Stack 속에 Stack도 얼마든지 가능합니다.
 struct CardView: View {
     var image: String
     var category: String
@@ -62,6 +69,8 @@ struct CardView: View {
                 .frame(width: 200,
                        height: 200,
                        alignment: .center)
+            //frame 을 카드 종류에 맞게 조정해야합니다!
+            //키워드 GeometryReader 사용하면 좋습니다. 하지만 여유가 없으시면 지금처럼 숫자로 값을 조정해주세요.
                 .padding()
             
             HStack{
@@ -81,15 +90,16 @@ struct CardView: View {
                 .layoutPriority(100)
                 
                 Spacer()
+                
             }
             .padding()
         }
         .cornerRadius(10)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
-                )
-                .padding([.top, .horizontal])
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
     }
 }
 
