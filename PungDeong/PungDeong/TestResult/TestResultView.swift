22e3b1f5eef5a4da6491950aeac3ee6437a3c0c3
Apple-Kong <kongthn@gmail.com>
2022-04-07 15:18:09 +0900

    메인화면 버튼, 공유버튼

--- PungDeong/PungDeong/TestResult/TestResultView.swift
+++ PungDeong/PungDeong/TestResult/TestResultView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+
+
 struct ResultData: Identifiable {
     let id: String
     let score: Double
@@ -26,7 +28,7 @@ struct TestResultView: View {
     
     var body: some View {
         VStack {
-            Text("당신의 결과는")
+            Text("당신의 결과는...")
                 .font(.title2)
                 .fontWeight(.bold)
                 .padding()
@@ -34,27 +36,75 @@ struct TestResultView: View {
             Text("전문가에게 물어보는 물개")
                 .font(.title)
                 .fontWeight(.bold)
+                .foregroundColor(Color("main"))
+                
             
             Image("test-image")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 200, height: 200)
+                .frame(width: 240, height: 240)
+                .overlay(Circle().stroke(.gray, lineWidth: 1))
+                .padding(.vertical)
+                
+               
             
             ForEach(resultData) { result in
                 ProgressView("\(result.id)", value: result.score, total: 10)
-                    .padding(.horizontal)
+                    .padding(.horizontal, 20)
                     .progressViewStyle(ResultProgressStyle())
             }
             
             Spacer()
+            
+            HStack {
+                Button {
+                    print("DEBUG: Share Button has tapped")
+                } label: {
+                    HStack {
+
+
+//                        Image(systemName: "square.and.arrow.up")
+//                            .resizable()
+//                            .aspectRatio(contentMode: .fill)
+                            
+                            
+                    }
+                    .padding()
+                    .frame(width: 50, height: 50)
+                    .background(Color("main"))
+                    .cornerRadius(25)
+                }
+                .padding(.horizontal, 6)
+                
+                
+                
+                Button {
+                    print("DEBUG: Button has tapped")
+                } label: {
+                    HStack {
+                        Text("메인화면으로 가기")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                    }
+                    .padding()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color("main"))
+                    .cornerRadius(10)
+                
+                }
+            }
         }
     }
 }
+
+//MARK: - 프로그레스 바 뷰 스타일
 
 struct ResultProgressStyle: ProgressViewStyle {
     func makeBody(configuration: Configuration) -> some View {
         ProgressView(configuration)
             .shadow(color: .black.opacity(0.3), radius: 4, x: 0, y: 4)
+            .tint(Color("main"))
     }
 }
 
