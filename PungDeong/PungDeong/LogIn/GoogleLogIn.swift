6e83aeec1e29f25639662f8d7e5ba0cb05e4c14f
김상현 <kshy1019@naver.com>
2022-04-07 16:04:06 +0900

    GoogleLogIn 생성, ContentView 반영

---
+++ PungDeong/PungDeong/LogIn/GoogleLogIn.swift
@@ -0,0 +1,20 @@
+//
+//  GoogleLogIn.swift
+//  PungDeong
+//
+//  Created by 김상현 on 2022/04/07.
+//
+
+import SwiftUI
+
+struct GoogleLogIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GoogleLogIn_Previews: PreviewProvider {
+    static var previews: some View {
+        GoogleLogIn()
+    }
+}
