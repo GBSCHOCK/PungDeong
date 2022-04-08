ab7f876326c554a92098ebb493f1bd59b7205357
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-04-08 15:27:03 +0900

    폰트 색상 및 버튼 생상 변경

--- PungDeong/PungDeong/Settings/SettingView.swift
+++ PungDeong/PungDeong/Settings/SettingView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct SettingView: View {
-    let menus: [String : String] = ["연결 계정 관리" : "arrow.triangle.2.circlepath","푸시 알림 설정" : "bell","다크 모드 설정" : "macmini","공지사항" : "gear"]
+    let menus: [String : String] = ["연결 계정 관리" : "arrow.triangle.2.circlepath","푸시 알림 설정" : "bell","다크 모드 설정" : "macmini","공지사항" : "info.circle"]
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
                 VStack {
                     VStack {
                         List {
-                            Section(header: ListHeader().frame(width: geometry.size.width, alignment: .leading).background(Color.blue).padding(-6)) {
+                            Section(header: ListHeader().frame(width: geometry.size.width, alignment: .leading).background(Color("VeryLightBlue")).padding(-6)) {
                                 ForEach(menus.sorted(by: >), id: \.key) { key, value in
                                     NavigationLink(
                                         destination: DetailView(data: key)) {
@@ -70,7 +70,7 @@ struct ListHeader: View {
     var body: some View {
         VStack {
             Text("계정")
-                .foregroundColor(Color.black)
+                .foregroundColor(Color("CustomGray"))
                 .font(.system(size: 25, weight: .bold))
                 .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
         }
@@ -84,7 +84,7 @@ struct SettingButtonStyle: ButtonStyle {
             .frame(width: 150, height: 45)
             .font(.system(size: 14))
             .foregroundColor(Color.white)
-            .background(Color.blue)
+            .background(Color("LightBlue"))
             .cornerRadius(6.0)
     }
 }
