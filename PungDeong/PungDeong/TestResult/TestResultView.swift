0ac4a264d2245025b0d2c39d1b5c5dbaa0fe6bd2
Apple-Kong <kongthn@gmail.com>
2022-04-08 15:47:32 +0900

    테스트 결과 데이터 스트럭트 생성

--- PungDeong/PungDeong/TestResult/TestResultView.swift
+++ PungDeong/PungDeong/TestResult/TestResultView.swift
@@ -10,19 +10,48 @@ import SwiftUI
 
 
 struct ResultData: Identifiable {
-    let id: String
+    
+    let id: Int
     let score: Double
+    
+    var description: String? {
+        
+        switch self.id {
+        case 1:
+            return "정보 재확인"
+        case 2:
+            return "근거 조사"
+        case 3:
+            return "전문가 자문"
+        case 4:
+            return "사실 재확인"
+        case 5:
+            return "중립"
+        default:
+            print("DEBUG: 잘못된 타입 값입니다.")
+            return nil
+        }
+    }
 }
+
+
+
 
 struct TestResultView: View {
     
+    
+    //데이터 건네져 옴
     @State private var resultData: [ResultData] = [
-        ResultData(id: "정보 확인형", score: 3),
-        ResultData(id: "근거 조사", score: 8),
-        ResultData(id: "전문가 자문", score: 6),
-        ResultData(id: "사실 재확인", score: 2),
-        ResultData(id: "중립성", score: 4)
+        ResultData(id: 1, score: 3),
+        ResultData(id: 2, score: 8),
+        ResultData(id: 3, score: 6),
+        ResultData(id: 4, score: 2),
+        ResultData(id: 5, score: 4)
     ]
+    
+    @State private var type: Int = 0
+    
+    
     
     var body: some View {
         VStack {
@@ -49,34 +78,49 @@ struct TestResultView: View {
                 }
                 
                 
-                ForEach(resultData) { result in
-                    
-                    HStack {
-                        VStack(alignment: .trailing) {
-                            Text("\(result.id)")
-                                .fontWeight(.semibold)
-                                
-                        }
-                        .padding(.leading)
+                VStack {
+                    ForEach(resultData) { result in
                         
-                        Spacer()
-                        
-                        ZStack {
-                            ProgressView(value: result.score, total: 10)
-                                .frame(width: 230,height: 20)
-                                .padding(.trailing, 30)
-                                .tint(result.score > 5 ? Color.pink : Color("main"))
-                            .progressViewStyle(ResultProgressStyle())
+                        HStack {
+                            Spacer()
                             
+                            Text("\(result.description ?? "")")
+                                .fontWeight(.semibold)
+                                .frame(alignment: .trailing)
+                                .padding(.horizontal)
+                            
+                            
+                            
+                            
+                            ZStack {
+                                ProgressView(value: result.score, total: 10)
+                                    .frame(width: 150,height: 20)
+                                    .padding(.trailing, 30)
+                                    .progressViewStyle(ResultProgressStyle())
+                                    .tint(result.score > 5 ? Color.pink : Color("main"))
+                                
+                            }
                         }
                     }
                 }
+                .frame(alignment: .center)
+                .offset(x: -40, y: 0)
+                
+                HStack {
+                    Text("#교수님사랑 #대학원생운명")
+                        .fontWeight(.bold)
+                        .padding(.leading, 20)
+                
+                    Spacer()
+                }
+                .padding(.top, 10)
                 
                 
                 Text("당신은 사실여부가 확실하지 않을 때는 반드시 전문가에게 찾아가고야 마는 대학원생 물개시군요!. 불확실한 정보 보다는 교수님의 말을 전적으로 믿으시는 당신!")
                     .lineLimit(3)
                     .font(.body)
-                    .padding()
+                    .padding(.horizontal, 20)
+                    .padding(.top, 1)
                 .multilineTextAlignment(.leading)
                     
                 
@@ -122,9 +166,7 @@ struct TestResultView: View {
                 }
             }
         }
-        
     }
-    
 }
 
 
@@ -133,25 +175,31 @@ struct TestResultView: View {
 
 struct ResultProgressStyle: ProgressViewStyle {
     func makeBody(configuration: Configuration) -> some View {
-        ZStack(alignment: .leading) {
-                   RoundedRectangle(cornerRadius: 5)
-                       .frame(width: 250, height: 10)
-                       .foregroundColor(.gray.opacity(0.1))
-                       .overlay(Color.black.opacity(0.5)).cornerRadius(14)
-                   
-                   RoundedRectangle(cornerRadius: 5)
-                       .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * 250, height: 10)
-                       .foregroundColor(Color("main"))
+        GeometryReader { geometry in
             
-            //반응형 UI 로 로직 수정 필요
-            ForEach(1..<6) { i in
-                Rectangle()
-                    .frame(width: 2, height: 20)
-                    .foregroundColor(.white)
-                    .offset(x: CGFloat(-2 + i * 40))
+            let width: CGFloat = geometry.size.width
+            
+            ZStack(alignment: .leading) {
+                       RoundedRectangle(cornerRadius: 5)
+                           .frame(width: width, height: 10)
+                           .foregroundColor(.gray.opacity(0.1))
+                           .overlay(Color.black.opacity(0.5)).cornerRadius(14)
+                       
+                       RoundedRectangle(cornerRadius: 5)
+                           .frame(width: CGFloat(configuration.fractionCompleted ?? 0) * width, height: 10)
+                           .foregroundColor(Color("main"))
+                
+                //반응형 UI 로 로직 수정 필요
+                ForEach(1..<5) { i in
+                    Rectangle()
+                        .frame(width: 1.5, height: 20)
+                        .foregroundColor(.white)
+                        .offset(x: CGFloat(-2 + i * Int(width) / 5))
+                }
             }
+            
         }
-        .padding()
+        
     }
 }
 
