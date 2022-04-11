2d2449b0720856339833a16fce16b6bfdb2cbad9
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-04-11 20:38:44 +0900

    임시저장

--- PungDeong/PungDeong/Settings/ProfileView.swift
+++ PungDeong/PungDeong/Settings/ProfileView.swift
@@ -9,33 +9,34 @@ import SwiftUI
 
 struct ProfileView: View {
     @State var profileInfo = ["안경 쓴 물개","아직 뭐하는 애인지 잘 모름."]
-    @State var testResult = ["테스트 결과","시뮬레이션 게임 결과"]
+    @State var testResult = ["테스트 결과: ","시뮬레이션 게임 결과: "]
+    @State private var showDetails = false
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 VStack {
                     Image("sample")
                         .resizable()
-                        .frame(width: 150, height: 150)
+                        .frame(width: geometry.size.width*0.5, height: geometry.size.width*0.5)
                     Text("#!@$!#$한 !@$!$@$이에요.")
                 }
-                .padding([.top, .bottom], 50)
+                .padding([.top, .bottom], geometry.size.height*0.05)
                 VStack {
                     VStack {
                         Text("프로필")
                             .foregroundColor(Color("CustomGray"))
                             .font(.system(size: 25, weight: .bold))
-                            .frame(width: geometry.size.width, height: 50, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.07, alignment: .leading)
                             .padding([.leading])
                     }
                     //.frame(width: geometry.size.width, height: 50)
                     .background(Color("VeryLightBlue"))
                     VStack {
                         Text("\(profileInfo[0])")
-                            .frame(width: geometry.size.width, height: 30, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.05, alignment: .leading)
                             .padding([.leading])
                         Text("\(profileInfo[1])")
-                            .frame(width: geometry.size.width, height: 30, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.05, alignment: .leading)
                             .padding([.leading])
                     }
                 }
@@ -44,32 +45,34 @@ struct ProfileView: View {
                         Text("지난 결과 모아보기")
                             .foregroundColor(Color("CustomGray"))
                             .font(.system(size: 25, weight: .bold))
-                            .frame(width: geometry.size.width, height: 50, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.07, alignment: .leading)
                             .padding([.leading])
                     }
                     //.frame(width: geometry.size.width, height: 50)
                     .background(Color("VeryLightBlue"))
                     VStack {
                         Text("\(testResult[0])")
-                            .frame(width: geometry.size.width, height: 30, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.05, alignment: .leading)
                             .padding([.leading])
                         Text("\(testResult[1])")
-                            .frame(width: geometry.size.width, height: 30, alignment: .leading)
+                            .frame(width: geometry.size.width, height: geometry.size.height*0.05, alignment: .leading)
                             .padding([.leading])
                     }
                 }
-                .padding([.bottom], 50)
+                .padding([.bottom], geometry.size.height*0.03)
                 VStack {
                     Button(action: {
                         
                     }, label: {
                         Text("테스트 결과 다시보기")
-                    }).buttonStyle(SettingButtonStyle())
+                    })
+                    .buttonStyle(SettingButtonStyle())
                     Button(action: {
                         
                     }, label: {
                         Text("로그아웃")
-                    }).buttonStyle(SettingButtonStyle())
+                    })
+                    .buttonStyle(SettingButtonStyle())
                     Button(action: {
                         
                     }, label: {
