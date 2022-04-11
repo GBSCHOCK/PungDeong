5b7e286685c9260f81c42adbcbbee81fe57e0355
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-04-11 15:13:45 +0900

    ContentView 수정

--- PungDeong/PungDeong/ContentView.swift
+++ PungDeong/PungDeong/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello, world!")
-//            .padding()
-        SettingView()
+        Text("Hello, world!")
+            .padding()
     }
 }
 
